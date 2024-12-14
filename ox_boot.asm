3a25d27d7dfe23eb7df4e8ff64e32e22693801e8
mihinN <anonnimnaka@gmail.com>
2024-12-14 22:57:27 +0530

    updated: ox_boot

--- ox_boot.asm
+++ ox_boot.asm
@@ -1,14 +1,56 @@
+disk_load: 
+    push dx
+
+    mov ah, 0x02
+    mov al, dh
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02
+
+    int 0x13
+
+    jc disk_error
+
+    pop dx
+    cmp dh, al
+    jne disk_error
+    ret
+
+disk_error: 
+    mov bx, DISK_ERROR_MSG
+    call print_string
+    jmp $
+
+DISK_ERROR_MSG db "Disk read ERROR!", 0
+
 [org 0x7c00]
 
-mov ah, 0x0e
+    mov [BOOT_DRIVE], dl
 
-mov al, 'x'
-int 0x10
+    mov bp, 0x8000
+    mov sp, bp
 
-jmp $
+    mov bx, 0x9000
+    mov dh, 5
+    mov dl, [BOOT_DRIVE]
+    call disk_load
 
-times 510 - ($ - $$) db 0
-dw 0xaa55
+    mov dx, [0x9000]
+    call print_hex
+
+    mov dx, [0x9000 + 512]
+    call print_hex
+
+    jmp $
+
+BOOT_DRIVE: 
+    db 0
+
+    times 510 - ($ - $$) db 0 
+    dw 0xaa55 ; Boot signature
+
+    times 256 dw 0xdada
+    times 256 dw 0xface
 
 
 
